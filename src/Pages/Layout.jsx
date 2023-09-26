05db53dcd9035f8980c7f219e717b79fc18fcfe4
Github-Archives <drewreynerwilliams@gmail.com>
2023-09-26 17:13:21 -0400

    replace react-router-dom Link with NavLink and begin utilizing useMatch

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,21 +1,58 @@
-import { Outlet, Link } from "react-router-dom"
+import React from "react"
+import { NavLink, Outlet, useMatch } from "react-router-dom"
+import "./Layout.css" // Import your CSS file
 
 const Layout = () => {
+  const homeMatch = useMatch("/")
+  const blogsMatch = useMatch("/blogs")
+  const contactMatch = useMatch("/contact")
+  const utilitiesMatch = useMatch("/utilities")
+
   return (
     <>
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <NavLink
+              to="/"
+              className={
+                homeMatch ? "nav-link-active nav-link-home" : "nav-link"
+              }
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to="/blogs">Blogs</Link>
+            <NavLink
+              to="/blogs"
+              className={
+                blogsMatch ? "nav-link-active nav-link-blogs" : "nav-link"
+              }
+            >
+              Blogs
+            </NavLink>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <NavLink
+              to="/contact"
+              className={
+                contactMatch ? "nav-link-active nav-link-contact" : "nav-link"
+              }
+            >
+              Contact
+            </NavLink>
           </li>
           <li>
-            <Link to="/utilities">Utilities</Link>
+            <NavLink
+              to="/utilities"
+              className={
+                utilitiesMatch
+                  ? "nav-link-active nav-link-utilities"
+                  : "nav-link"
+              }
+            >
+              Utilities
+            </NavLink>
           </li>
         </ul>
       </nav>
