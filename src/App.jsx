865d5d393dcfc421914cf98acc76b16bc8be00da
Github-Archives <drewreynerwilliams@gmail.com>
2023-09-26 02:01:33 -0400

    working router

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,49 @@ import { useState } from "react"
 import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
 import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom" // wrap around elements you want to use routing on
+import Layout from "./Pages/Layout"
+import Home from "./Pages/Home"
+import Blogs from "./Pages/Blogs"
+import Contact from "./Pages/Contact"
+import NoPage from "./Pages/NoPage"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>☁ Portfolio ☁</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Count is now at {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <BrowserRouter>
+      <>
+        <div>
+          <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank" rel="noreferrer">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>☁ Portfolio ☁</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            Count is now at {count}
+          </button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="blogs" element={<Blogs />} />
+              <Route path="contact" element={<Contact />} />
+              <Route path="*" element={<NoPage />} />
+            </Route>
+          </Routes>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </>
+    </BrowserRouter>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import App from "./App"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
+  // ! lookup React StrictMode. it was here because we used create-react-app and is why things are double rendering.. to take this away gets rid of checks that show me i've done bad practices with the lifecycle. also, it only double renders in dev, not prod apparently !
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 )
