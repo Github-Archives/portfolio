804ff61fcb79c56a60b1cf30b760e032a10dd2e2
Github-Archives <drewreynerwilliams@gmail.com>
2023-09-30 01:17:14 -0400

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-
-  theme: {
-    extend: {},
+  theme: { // Putting styles overrides everything, not just add your new values
+    extend: {
+      colors: {
+        "twitter-blue": "#1DA1F2",
+    },
+    },
   },
   plugins: [],
 }
