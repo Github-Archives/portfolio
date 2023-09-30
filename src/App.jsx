3d7b67cce732dd85cd273fda511d9bc922515089
Drew Williams <15261678+Github-Archives@users.noreply.github.com>
2023-09-30 16:25:37 -0400

    Implement react-rough-notation and upgrade yarn packages (#7)
    * add rough-notation
    
    * update styles for better rough-notation visibility
    
    * remove rough-notation
    
    * yarn add react-rough-notation
    
    * yarn upgrade pkgs and implement simple react-rough-notation action

--- src/App.jsx
+++ src/App.jsx
@@ -55,6 +55,7 @@ function App() {
             </>
           }
         </div>
+        <br />
         <VerticalSpace height="600px" />
         <div id="section2">
           {
@@ -65,6 +66,7 @@ function App() {
             </>
           }
         </div>
+        <br />
         {/* Smooth Scrolling */}
 
         <div className="rounded-lg bg-white px-6  py-8 shadow-xl ring-1 ring-slate-900/5 dark:bg-slate-800">
@@ -83,10 +85,10 @@ function App() {
           <h3 className="mt-5 text-base font-medium tracking-tight text-slate-900 dark:text-white">
             Writes Upside-Down
           </h3>
-          <p className="mt-2 text-sm text-slate-500 dark:text-slate-400">
+          <code className="mt-2 text-sm text-slate-500 dark:text-slate-400">
             The Zero Gravity Pen can be used to write in any orientation,
             including upside-down. It even works in outer space.
-          </p>
+          </code>
         </div>
       </div>
     </BrowserRouter>
