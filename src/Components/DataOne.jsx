fe59281b5d36baca82a3a3e2a2d36b9701e90141
Github-Archives <drewreynerwilliams@gmail.com>
2023-09-30 13:53:32 -0400

    update styles for better rough-notation visibility

--- src/Components/DataOne.jsx
+++ src/Components/DataOne.jsx
@@ -2,28 +2,35 @@ import React from "react";
 
 const DataOne = () => {
   return (
-    <section className="App-header" id="DataOne">
-      <div>DataOne Section</div>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Habitant morbi
-        tristique senectus et netus et malesuada fames. Quam pellentesque nec
-        nam aliquam. Ac odio tempor orci dapibus ultrices in. At varius vel
-        pharetra vel turpis nunc eget. Eget mauris pharetra et ultrices. Lectus
-        vestibulum mattis ullamcorper velit.
-      </p>
-      <p>
-        At varius vel pharetra vel turpis nunc eget lorem. Eu ultrices vitae
-        auctor eu. Nulla facilisi etiam dignissim diam quis enim. Euismod
-        lacinia at quis risus sed. Maecenas sed enim ut sem viverra aliquet.
-        Sapien nec sagittis aliquam malesuada. Aliquam faucibus purus in massa
-        tempor nec. Purus non enim praesent elementum facilisis leo vel.
-        Bibendum at varius vel pharetra vel. Orci porta non pulvinar neque
-        laoreet. Diam ut venenatis tellus in metus vulputate eu. Dictum non
-        consectetur a erat nam at. Amet risus nullam eget felis.
-      </p>
+    <section
+      className="App-header rounded-lg bg-white px-6  py-8 shadow-xl ring-1 ring-slate-900/5 dark:bg-slate-800"
+      id="DataOne"
+    >
+      <h3 className="mt-5 text-base font-medium tracking-tight text-slate-900 dark:text-white">
+        DataOne Section
+      </h3>
+      <div className="text-left">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitant
+          morbi tristique senectus et netus et malesuada fames. Quam
+          pellentesque nec nam aliquam. Ac odio tempor orci dapibus ultrices in.
+          At varius vel pharetra vel turpis nunc eget. Eget mauris pharetra et
+          ultrices. Lectus vestibulum mattis ullamcorper velit.
+        </p>
+        <p>
+          At varius vel pharetra vel turpis nunc eget lorem. Eu ultrices vitae
+          auctor eu. Nulla facilisi etiam dignissim diam quis enim. Euismod
+          lacinia at quis risus sed. Maecenas sed enim ut sem viverra aliquet.
+          Sapien nec sagittis aliquam malesuada. Aliquam faucibus purus in massa
+          tempor nec. Purus non enim praesent elementum facilisis leo vel.
+          Bibendum at varius vel pharetra vel. Orci porta non pulvinar neque
+          laoreet. Diam ut venenatis tellus in metus vulputate eu. Dictum non
+          consectetur a erat nam at. Amet risus nullam eget felis.
+        </p>
+      </div>
       <a
-        className="App-link"
+        className="App-link mt-5 text-base font-medium tracking-tight text-slate-900 dark:text-white"
         href="https://reactjs.org"
         target="_blank"
         rel="noopener noreferrer"
