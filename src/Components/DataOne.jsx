6e23688a6e7d2c0da8cff16bdabc6a825d6cbd79
Github-Archives <drewreynerwilliams@gmail.com>
2023-09-30 16:22:59 -0400

    yarn upgrade pkgs and implement simple react-rough-notation action

--- src/Components/DataOne.jsx
+++ src/Components/DataOne.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import { useState } from "react";
+import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 
 const DataOne = () => {
+  const [showRoughNotation, setShowRoughNotation] = useState(false);
+
   return (
     <section
       className="App-header rounded-lg bg-white px-6  py-8 shadow-xl ring-1 ring-slate-900/5 dark:bg-slate-800"
       id="DataOne"
     >
+      <button onClick={() => setShowRoughNotation(!showRoughNotation)}>
+        Toggle RoughNotation
+      </button>
+
+      {showRoughNotation && (
+        <RoughNotation type="underline" show={showRoughNotation}>
+          <br />
+          Hello RoughNotation
+        </RoughNotation>
+      )}
+
+      {/* Hard-coded state (showRoughNotation needs to be set to true) */}
+      {/* <RoughNotation type="underline" show={showRoughNotation}>
+        Hello RoughNotation
+      </RoughNotation> */}
+
       <h3 className="mt-5 text-base font-medium tracking-tight text-slate-900 dark:text-white">
         DataOne Section
       </h3>
+
       <div className="text-left">
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
