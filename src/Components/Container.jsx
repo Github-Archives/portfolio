ff54665909820e4301ea7e6884ac92709f78021a
Github-Archives <drewreynerwilliams@gmail.com>
2024-07-02 20:06:23 -0400

    add Container element to embed About, Projects, and Blog components

---
+++ src/Components/Container.jsx
@@ -0,0 +1,17 @@
+import About from './About'
+import Projects from './Projects'
+import Blog from './Blog'
+
+// ? Idk wtf this is even doing
+const Container = () => {
+  return (
+    <div>
+      <div className="Container-Test">Container-Visual-Test</div>
+      <About />
+      <Projects />
+      <Blog />
+    </div>
+  )
+}
+
+export default Container
